104a7087f889a232f6945c9a4c05bd48fa7e4d3b
Valerio Minetti <valerio.minetti@gmail.com>
2013-04-22 14:39:00 +0200

    initial commit of bot file
    file added

---
+++ ffmpeg-bot.php
@@ -0,0 +1,77 @@
+<?php
+	// FFmpeg encoder Bot for MediaCore
+	// Code by Valerio Minetti 	
+	// valerio.minetti@gmail.com
+
+	// Bot Loop
+
+while(true){
+	// Db connect (change your connection credentials)
+	$link = mysql_connect('localhost', 'root', 'root');
+	if (!$link) {
+	    die('Could not connect: ' . mysql_error());
+	}
+	mysql_select_db('mediacore', $link);
+	$select_query = "select media.id as mediaid,media_files.id,media_files.type,container,unique_id,media_files.storage_id from media_files left join media on (media_files.media_id = media.id) where (media.encoded=0) and (media_files.storage_id=1);";
+	$unencoded_media = mysql_query($select_query) or die(mysql_error());
+	//echo 'Connected successfully';
+
+	// print unencoded media
+	while($row = mysql_fetch_assoc($unencoded_media)){
+	    foreach($row as $cname => $cvalue){
+        	print "$cname: $cvalue\t";
+	    }
+	    print "\r\n";
+
+	    $media_file_name= $row['unique_id'];
+	    $media_id = $row['mediaid'];
+	    // insert new media file into mediacore tables
+	    // get width & height
+		exec("ffmpeg -i /opt/mediacore/data/media/".$media_file_name." 2>&1 |grep '\[PAR'", $out);
+                $dimension = explode(" ", trim(current(explode('[PAR', current($out)))))[6];
+		$width= explode("x",$dimension)[0];
+		$height= explode("x",$dimension)[1];
+	    // get bitrate
+		exec("ffmpeg -i /opt/mediacore/data/media/".$media_file_name." 2>&1 |grep 'bitrate:'", $out1);
+		$brate = trim(str_replace('bitrate:', NULL, end(explode(',', current($out1)))));
+		//echo "bitrate";
+		//print_r($brate);
+
+	    // extract filename without extension
+	    // TODO: encoding presets support
+	    $encoded_bitrate = "800k";
+	    $encoded_width = 640;
+	    $encoded_height = 360;
+    	    $encoded_file_name = explode("-",pathinfo($media_file_name,PATHINFO_FILENAME))[1]."-". $encoded_width."x".$encoded_height."-".$encoded_bitrate.".mp4";
+	    echo $encoded_file_name."\n";
+
+	    // transcode file
+	    // TODO: support for multiple profiles
+	    echo "Starting ffmpeg with ".$media_file_name."...\n\n";
+	    echo shell_exec("ffmpeg -y -i /opt/mediacore/data/media/".$media_file_name." -pass 1  -vcodec libx264  -vpre medium -b ".$encoded_bitrate." -s ".$encoded_width."x".$encoded_height." -acodec libmp3lame -ac 2 -ar 48000 -ab 128k   /opt/mediacore/data/media/".$encoded_file_name." </dev/null >/dev/null 2>/var/log/ffmpeg.log ");
+	    echo "Encoding Done.\n";
+
+	    // insert media file in mediacore db
+	    $insert_query="insert into mediacore.media_files ( media_id, storage_id, display_name, unique_id,size, created_on,modified_on,bitrate, width, height, type, container ) values ('".$media_id."','1','".$encoded_file_name."','".$encoded_file_name."','".filesize("/opt/mediacore/data/media/". $encoded_file_name)."','','','".$encoded_bitrate."','".$encoded_width."','".$encoded_height."', 'video', 'mp4')";
+	    $result = mysql_query($insert_query) or die(mysql_error());
+
+	    // TODO: figure out media_file id prior db insert, avoiding update
+	    // get file id
+	    $select_query="select id from mediacore.media_files where unique_id='".$encoded_file_name."'";
+	    $result = mysql_query($select_query) or die(mysql_error());
+	    $new_id = mysql_fetch_assoc($result);
+	    // rename and update transcoded media file and db row
+	    rename("/opt/mediacore/data/media/".$encoded_file_name, "/opt/mediacore/data/media/".$new_id['id']."-".$encoded_file_name );
+	    $update_query= "update mediacore.media_files set unique_id='".$new_id['id']."-".$encoded_file_name."' where id='".$new_id['id']."';";
+	    $update_query1="update mediacore.media set encoded='1' where id='".$media_id."';";
+	    $result = mysql_query($update_query) or die(mysql_error());
+	    $result = mysql_query($update_query1) or die(mysql_error());
+
+	}
+	
+	mysql_close($link);
+
+	// Sleep till next check
+	sleep(10);
+}
+?>
